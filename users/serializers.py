4236e6dae4e6321918d89dc046ef6b203cc2427a
Egor Volkov <volkovqa96@gmail.com>
2023-09-06 20:19:02 +0300

    Разбил сериализатор пользователя, добавил историю платежей.

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework.serializers import ModelSerializer
 
+from courses.serializers import PaymentSerializer
 from users.models import User
 
 
-class UserSerializer(ModelSerializer):
+class UserListSerializer(ModelSerializer):
+    """
+    Вывод списка пользователей с их основными полями
+    """
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ["id", "email", "first_name", "last_name", "phone_number", "location", "is_active"]
+
+
+class UserDetailSerializer(ModelSerializer):
+    """
+    Детальный вывод одного пользователя. Кроме основных, есть вложенное поле истории платежей
+    """
+    payments = PaymentSerializer(source='payment_set', many=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name", "phone_number", "location", "is_active", "payments"]
 
